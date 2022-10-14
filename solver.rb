cac42049e66935dd74080e3d2b1987b43253f568
ijeoma <ijeomaodinko>
2022-10-14 18:28:23 -0400

    Add files.

--- solver.rb
+++ solver.rb
@@ -1,33 +1,29 @@
 class Solver
-    def factorial(num)
-      facto = 1
-      if num.class != Integer || num.negative?
-        'exception'
-      elsif num <= 1
-        1
-      else
-        (2..num).each do |i|
-          facto *= i
-        end
-        facto
+  def factorial(num)
+    facto = 1
+    if num.class != Integer || num.negative?
+      'exception'
+    elsif num <= 1
+      1
+    else
+      (2..num).each do |i|
+        facto *= i
       end
+      facto
     end
-    
-    def reverse
+  end
 
+  def reverse; end
 
-
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
+    else
+      num.to_s
     end
-    
-    def fizzbuzz(num)
-        if (num % 3).zero? && (num % 5).zero?
-          'fizzbuzz'
-        elsif (num % 3).zero?
-          'fizz'
-        elsif (num % 5).zero?
-          'buzz'
-        else
-          num.to_s
-        end
-      end
-    end 
+  end
+end
